5be06937d4c9547ceb68e4e9d8b79383dc814ce6
Arsalan Danish <arsalan.jamia@gmail.com>
2023-03-01 15:45:24 +0530

    DSA code -01

---
+++ dsa-06feb.js
@@ -0,0 +1,13 @@
+// # Program to check if a number is odd or even!!
+
+function checkOddEvenNumber(number) {
+    if(number%2==0){
+        console.log("The given number is an Even Number.");
+    }
+    else(
+        console.log("The given numbers is an Odd Number.")
+    )
+}
+console.log(checkOddEvenNumber(13));
+
+// The given numbers is an Odd Number.
