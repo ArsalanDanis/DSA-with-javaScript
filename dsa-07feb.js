b76c21bcc9ba364883621938410cc84bbd0e89e5
Arsalan Danish <arsalan.jamia@gmail.com>
2023-03-20 21:06:03 +0530

    dsa 07feb assignment

---
+++ dsa-07feb.js
@@ -0,0 +1,9 @@
+// # Program to Add Two Numbers !
+
+function addTwoNumbers(Number1, Number2) {
+    let sum = (Number1 + Number2);
+    return sum;
+}
+console.log("The summation of Number1 and Number2 is equal to =", addTwoNumbers(15, 63));
+
+// The summation of Number1 and Number2 is equal to = 78 
