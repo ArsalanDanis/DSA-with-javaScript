5705b0d221e44063b757f0483e5acbbf06f8e5e3
Arsalan Danish <arsalan.jamia@gmail.com>
2023-03-29 00:50:19 +0530

    dsa 08feb assignment

---
+++ dsa-08feb.js
@@ -0,0 +1,8 @@
+// # Program to Find the Square Root of given Number!!
+
+function squareRootOfNumber(Number) {
+    return Math.sqrt(Number);
+}
+console.log("The Square root of a given Number is equal to =", squareRootOfNumber(81));
+
+// The Square root of a given Number is equal to = 9 
