d9cbcc6101e66b2796ac12b3f530578c38e836d3
Arsalan Danish <arsalan.jamia@gmail.com>
2023-03-31 01:25:22 +0530

    dsa 14feb assignment

---
+++ dsa-14feb.js
@@ -0,0 +1,9 @@
+// Program to find cube root of a number ? 
+
+function cubeRootOfNumber(number) {
+
+    return Math.cbrt(number);
+}
+console.log("The Cube root of 1000 is equal to =", cubeRootOfNumber(1000));
+
+// The Cube root of 1000 is equal to = 10 
