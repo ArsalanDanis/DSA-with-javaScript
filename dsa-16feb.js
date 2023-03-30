1dcdcbdc5643c2edf2d65964b801209db766fb6f
Arsalan Danish <arsalan.jamia@gmail.com>
2023-03-31 01:28:19 +0530

    dsa 16feb assignment

---
+++ dsa-16feb.js
@@ -0,0 +1,20 @@
+// Program to check if a number is Positive,Negative or Zero !!!
+
+function checkTheNumber(number) {
+    if (number > 0) {
+        console.log("The given number is a Positive Number.")
+    }
+    else if (number < 0) {
+        console.log("The given number is a Negative Number.")
+    }
+    else {
+        console.log("The given number is Zero.")
+    }
+}
+checkTheNumber(156);
+//  The given number is a Positive Number.
+checkTheNumber(-75);
+// The given number is a Negative Number.
+checkTheNumber(0);
+// The given number is Zero.
+
