0602d03d533448f6be78114f315576a2da32930e
Arsalan Danish <arsalan.jamia@gmail.com>
2023-03-31 01:23:49 +0530

    dsa 13feb assignment

---
+++ dsa-13feb.js
@@ -0,0 +1,9 @@
+// # Program to convert Km to Miles !!
+
+function kmToMiles(km) {
+    let miles = km * 0.621;
+    return miles;
+}
+console.log("1 km is equal to =", kmToMiles(1),"mile.");
+
+// 1 km is equal to = 0.621 mile.
