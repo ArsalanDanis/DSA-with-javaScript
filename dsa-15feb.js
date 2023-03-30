fdec9804fd840aeba9b55f8f58ba16d575b4aad6
Arsalan Danish <arsalan.jamia@gmail.com>
2023-03-31 01:26:49 +0530

    dsa 15feb assignment

---
+++ dsa-15feb.js
@@ -0,0 +1,10 @@
+// Program to convert Celsius to Forenheit !!!
+
+function celsiusToForenheit(Celsius) {
+    let Forenheit = ((9 / 5 * Celsius) + 32);
+    return Forenheit;
+}
+console.log("100 °C is equal to =", celsiusToForenheit(100), "°F.");
+
+// 100 °C is equal to = 212 °F.
+
