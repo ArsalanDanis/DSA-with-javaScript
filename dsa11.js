3b5f5acc632be40095670d8bd63b1c116f207455
Arsalan Danish <arsalan.jamia@gmail.com>
2023-04-08 02:27:20 +0530

    dsa-11 code

---
+++ dsa11.js
@@ -0,0 +1,9 @@
+// Program to Calculate the Area of a Triangle !!!
+
+function areaOfTriangle(height, base) {
+    let area = (1 / 2) * height * base;
+    return area;
+}
+console.log("The area of a triangle is :", areaOfTriangle(25, 10.5),"square unit.");
+
+// The area of a triangle is : 131.25 square unit.
