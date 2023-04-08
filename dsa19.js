95dd20e5718b33adc3436a892e26e18f1b8169de
Arsalan Danish <arsalan.jamia@gmail.com>
2023-04-09 03:28:03 +0530

    DSA 19 Assignment

---
+++ dsa19.js
@@ -0,0 +1,16 @@
+//  # factors 
+
+function factorsOfNumbers(num){
+    for(i=1;i<num;i++){
+        if(num % i == 0){
+            console.log(i)
+        }
+    }
+}
+factorsOfNumbers(48);
+
+// 1,2,3,4,6,8,12,16,24 
+
+
+
+    
