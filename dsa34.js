dd1bb059e762af7347196f722e9ad807945d4269
Arsalan Danish <arsalan.jamia@gmail.com>
2023-04-09 03:47:20 +0530

    DSA 34 Assignment

---
+++ dsa34.js
@@ -0,0 +1,8 @@
+// Program to Sort Words in Alphabetical Order.
+
+let str="not able to find out the mistakes";
+let str1=str.split(" ").sort().join(" ");
+
+console.log(str1)
+
+// able find mistakes not out the to
