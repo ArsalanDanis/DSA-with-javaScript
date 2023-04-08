886d842192602f8cf85b402fe874560f5f7cc50e
Arsalan Danish <arsalan.jamia@gmail.com>
2023-04-09 04:02:21 +0530

    DSA 50 Assignment

---
+++ dsa50.js
@@ -0,0 +1,34 @@
+// JavaScript Program to Create Multiline Strings.
+
+// (1)Method-1 by using (+) 
+ 
+const str1 = "List of stationary items.\n"+"(1)Books\n"+"(2)Notebook\n"+"(3)Pen\n";
+console.log(str1);
+// List of stationary items.
+// (1)Books
+// (2)Notebook
+// (3)Pen
+
+// (1)Method-2 by using (\) 
+
+const str2 = "List of Fruits.\n \(1)Mango\n \(2)Banana\n \(3)Apple"
+console.log(str2);
+
+// List of Fruits.
+//  (1)Mango
+//  (2)Banana
+//  (3)Apple
+
+// (1)Method-3 by using (`backtick or Templet litrels`) 
+
+const str3 = `List of Colors.
+(1)Red
+(2)Green
+(3)Pink`
+console.log(str3);
+
+// List of Colors.
+// (1)Red
+// (2)Green
+// (3)Pink
+
