6298eb74a2f85f2168f83f5720916368ad736398
Arsalan Danish <arsalan.jamia@gmail.com>
2023-04-09 03:30:20 +0530

    DSA 22 Assignment

---
+++ dsa22.js
@@ -0,0 +1,10 @@
+// #Program to Find the Sum of Natural Numbers.
+
+let num = 10;
+let res = 0;
+for (i = 1; i <= num; i++) {
+    res += i;
+}
+console.log(`Sum of first ${num} natural no is :`, res);
+
+// Sum of first 10 natural no is : 55
